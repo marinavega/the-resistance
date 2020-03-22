060f99c07bbbdbab53ea54a13b2b6e1fb46af72f
Marina <marina@benditallave.com>
2020-03-22 16:43:38 +0100

    Add RadarController

--- app/controllers/radar_controller.rb
+++ app/controllers/radar_controller.rb
@@ -2,9 +2,12 @@ class RadarController < ApplicationController
   def radar
     payload = JSON.parse(request.raw_post)
 
-    attack_modes = payload['attack-mode'].map { |code| PositionParser.new.load(code) }
-    positions = TargetSelector::RadarPositionsFactory.new(payload['radar']).radar_positions
-    position = Selector.new(attack_modes, positions).position
+    attack_modes = payload['attack-mode'].map do |code|
+      TargetSelector::AttackModes::AttackSelector.new.select(code)
+    end
+
+    positions = TargetSelector::RadarFactory.new(payload['radar']).positions
+    position = TargetSelector::Selector.new(attack_modes, positions).position
 
     render json: position
   end
