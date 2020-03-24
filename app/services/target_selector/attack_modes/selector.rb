df7645b28cab3fd77297342304d7df14b7f78a1b
Marina <marina@benditallave.com>
2020-03-24 17:00:37 +0100

    Save and memoize attack modes as a const

--- app/services/target_selector/attack_modes/selector.rb
+++ app/services/target_selector/attack_modes/selector.rb
@@ -2,13 +2,10 @@ module TargetSelector
   module AttackModes
     class Selector
       MODULE = 'TargetSelector::AttackModes'.freeze
+      ATTACK_MODES = YAML.load_file('config/data/attack_modes.yml').freeze
 
       def call(attack_mode)
-        Object.const_get("#{MODULE}::#{mode_configs[attack_mode]}")
-      end
-
-      def mode_configs
-        @mode_configs ||= YAML.load_file('config/data/attack_modes.yml')
+        Object.const_get("#{MODULE}::#{ATTACK_MODES[attack_mode]}")
       end
     end
   end
